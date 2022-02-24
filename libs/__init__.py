c51a1c762f7a8c1bc11b0f8cf133d3b3412b66dc
wdw <boa3465@gmail.com>
2022-02-24 15:06:50 +0900

    잘못된 모델 경로 수정 패키지에서 모듈을 공개하는 방식 변경

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,7 +1,12 @@
 import os
 
 
-cur_dir = os.path.abspath(os.path.join(__file__))
+cur_dir = os.path.abspath(os.path.dirname(__file__))
 models_dir = os.path.abspath(os.path.join(cur_dir, "models"))
 
 embed_model_path = os.path.abspath(os.path.join(models_dir, "openface_nn4.small2.v1.t7"))
+
+
+from . import utils
+from . import detection
+from . import train_prediction
